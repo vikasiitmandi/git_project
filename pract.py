a14454f79b348d909237f77853a34bc248cb297b
vikasiitmandi <vikas1206iitmandi@gmail.com>
2024-08-22 13:13:25 +0530

    commitonfeat1

---
+++ pract.py
@@ -0,0 +1,10 @@
+def factorial_iterative(n):
+    if n < 0:
+        return "Factorial is not defined for negative numbers."
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+# Example usage
+print(factorial_iterative(5))  # Output: 120
