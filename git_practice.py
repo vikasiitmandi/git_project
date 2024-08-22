017c47909fba2446a95401127bc2a69b7cf2fbc0
vikasiitmandi <vikas1206iitmandi@gmail.com>
2024-08-22 11:53:56 +0530

    updated commit

--- git_practice.py
+++ git_practice.py
@@ -8,3 +8,4 @@ def multiply_all(*args):
 print(multiply_all(2, 3))          # Output: 6
 print(multiply_all(2, 3, 4))       # Output: 24
 print(multiply_all(1, 2, 3, 4, 5)) # Output: 120
+print('vikas Singh')
